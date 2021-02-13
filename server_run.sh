7870100b7845f6c8113a31be6c6dee6eca5948c4
Yash Agarwal <yagarwal1307@gmail.com>
2021-02-14 02:41:11 +0530

    Just a try fix

--- server_run.sh
+++ server_run.sh
@@ -1,10 +1,10 @@
-#Load nvm without restarting terminal
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+# #Load nvm without restarting terminal
+# export NVM_DIR="$HOME/.nvm"
+# [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+# [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 
-#Activate nvm
-. ~/.nvm/nvm.sh
+# #Activate nvm
+# . ~/.nvm/nvm.sh
 
 #Changing to server directory
 cd ./backend
