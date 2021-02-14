abf111935db8542ba8366f32c166ae73ef34a25d
Yash Agarwal <yagarwal1307@gmail.com>
2021-02-14 15:17:29 +0530

    Update server run file

--- server_run.sh
+++ server_run.sh
@@ -4,8 +4,11 @@
 #Changing to server directory
 cd ./backend
 
+#Create the .env file
+cp .env.example .env
+
 #Installing dependencies
 npm install
 
 #Starting server
-npm start
+npm run-script dev
