0bb1ec88009fa63104f5332012ef27eee7b72149
Yash Agarwal <yagarwal1307@gmail.com>
2021-02-14 18:10:25 +0530

    Remove URL and add it to env variables

--- frontend/src/axios-xmeme.js
+++ frontend/src/axios-xmeme.js
@@ -1,9 +1,8 @@
 //Axios instance with base URL declared
-
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: 'https://xmeme1307.herokuapp.com/'  //Replace it with your backend URL
+  baseURL:  process.env.REACT_APP_BACKEND_URL //Replace it with your backend URL
 });
 
 export default instance;
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,6 +5,7 @@ import './index.css';
 import App from './containers/App';
 import reportWebVitals from './reportWebVitals';
 
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
